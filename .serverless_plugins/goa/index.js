d4eef2da7fbd9b78468266d37b267e480ec42598
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2018-11-30 18:28:30 +0900

    Add dotenv

---
+++ .serverless_plugins/goa/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const dotenv = require('dotenv');
+dotenv.config({ path: '.env.local' });
+dotenv.config({ path: '.env' });
+
+
+class GoaPlugin {
+  constructor(serverless, options) {
+    this.serverless = serverless;
+    this.options = options;
+
+    this.commands = {};
+
+    this.hooks = {};
+  }
+}
+
+module.exports = GoaPlugin;
---
+++ lib/init_script.rb
@@ -0,0 +1,6 @@
+require 'rubygems'
+require 'bundler/setup'
+
+require 'dotenv'
+
+Dotenv.load '.env.local', '.env'
