bde2ead486bb2af43220d64a292b9cece4cbbde9
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2018-12-05 18:33:13 +0900

    Add jest

--- .serverless_plugins/goa/lib/env.js
+++ .serverless_plugins/goa/lib/env.js
@@ -5,8 +5,10 @@ const dotenv = require('dotenv');
 const ENV_FILE = '.env';
 const ENV_FILE_LOCAL = '.env.local';
 
-dotenv.config({ path: ENV_FILE_LOCAL });
-dotenv.config({ path: ENV_FILE });
+if (process.env.NODE_ENV !== 'test') {
+  dotenv.config({ path: ENV_FILE_LOCAL });
+  dotenv.config({ path: ENV_FILE });
+}
 
 module.exports = {
   ENV_FILE,
