c66b6ad63bcb1cf87fa57e98b5d9b6faa2ef5326
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2018-12-09 21:14:03 +0900

    Add react-router

--- web-client/src/App.tsx
+++ web-client/src/App.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import Home from './components/pages/Home';
 import SignIn from './components/pages/SignIn';
 import { signIn } from './lib/authentication';
 
-const App = () => <SignIn onLogin={signIn} />;
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/sign_in" render={() => <SignIn onLogin={signIn} />} />
+      <Route path="/" render={() => <Home />} />
+      <Route render={() => <Redirect to="/" />} />
+    </Switch>
+  </BrowserRouter>
+);
 
 export default App;
